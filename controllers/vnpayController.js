62ca50d9bb4b071b52fd7ce84d76834ca8fdfbf6
dthuyet <duythuyet2k3@gmail.com>
2025-05-20 23:03:07 +0700

    v4

--- controllers/vnpayController.js
+++ controllers/vnpayController.js
@@ -107,7 +107,7 @@ export const vnpayReturn = async (req, res) => {
     const hmac = crypto.createHmac("sha512", secretKey);
     const signed = hmac.update(Buffer.from(signData, 'utf-8')).digest("hex");
 
-    const clientReturnUrlBase = process.env.CLIENT_URL || 'http://localhost:3000';
+    const clientReturnUrlBase = process.env.CLIENT_URL || 'http://tvd2003.id.vn';
     let redirectUrl = `${clientReturnUrlBase}/payment-status`;
     let success = false;
     let message = '';
